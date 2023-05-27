2c0a6874813fd9571058fdc6c2c4273235fc767b
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-05-27 08:01:15 +0200

    script

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div style={{ 
+        backgroundColor: 'black', 
+        color: 'white', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '60px', 
+        position: 'fixed', 
+        left: '0',
+        bottom: '0',
+        width: '100%',
+    }}>
+        NewYorkTimes Clone - Daniele Caporaletti
+    </div>
   )
 }
 
